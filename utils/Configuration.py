e3853129d767ecc303ee6de08fac0c2d914f12c3
steeldragon960 <68459550+steeldragon960@users.noreply.github.com>
2022-01-16 08:14:11 +0800

    Added guild-id and force-global getters

--- utils/Configuration.py
+++ utils/Configuration.py
@@ -1,8 +1,17 @@
 import yaml
 
 FILE = "config.yml"
+
 config = yaml.load(open(FILE, encoding="utf-8"), Loader=yaml.FullLoader)
 
 
-def getConfig():
+def get() -> dict:
     return config
+
+
+def get_guild_ids() -> list:
+    return config["Guild-IDs"]
+
+
+def get_global() -> bool:
+    return config["Global-Slash-Commands"]
